8c50006e7967c37e0128e02118f2964326cc24dc
Live session user <Anoldius Paul>
2026-08-24 14:29:47 +0000

    Mabadiriko kwenye sales

--- sales/urls.py
+++ sales/urls.py
@@ -3,7 +3,11 @@ from rest_framework.routers import DefaultRouter
 from .views import SaleViewSet, CustomerViewSet, DebtViewSet, ExpenseViewSet
 
 router = DefaultRouter()
+
+# Sajili SaleViewSet chini ya 'transactions' na 'sales' ili kuzuia 404 Not Found
+router.register(r'transactions', SaleViewSet, basename='transaction')
 router.register(r'sales', SaleViewSet, basename='sale')
+
 router.register(r'customers', CustomerViewSet, basename='customer')
 router.register(r'debts', DebtViewSet, basename='debt')
 router.register(r'expenses', ExpenseViewSet, basename='expense')
