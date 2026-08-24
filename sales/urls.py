1364af3c0695512fcebf9d517a678d41073a3bca
Live session user <Anoldius Paul>
2026-08-24 10:41:03 +0000

    Kuongeza function ya matumizi

--- sales/urls.py
+++ sales/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SaleViewSet, CustomerViewSet, DebtViewSet,ExpenseViewSet
+from .views import SaleViewSet, CustomerViewSet, DebtViewSet, ExpenseViewSet
 
 router = DefaultRouter()
 router.register(r'transactions', SaleViewSet, basename='sale')
